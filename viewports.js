b7c5eb7f156ed9afafd664d313747a497c1afbc4
Jan Kiesewetter <jan@t3easy.de>
2020-09-18 03:51:50 +0200

    [FEATURE] Add prefers-color-scheme example

--- viewports.js
+++ viewports.js
@@ -1,17 +1,35 @@
 module.exports = [
     {
-        "label": "phone",
-        "width": 320,
-        "height": 480
+        label: "phone",
+        width: 320,
+        height: 480,
     },
     {
-        "label": "tablet",
-        "width": 1024,
-        "height": 768
+        label: "phone_dark",
+        width: 320,
+        height: 480,
+        prefersColorScheme: "dark"
     },
     {
-        "label": "fhd",
-        "width": 1920,
-        "height": 1080
+        label: "tablet",
+        width: 1024,
+        height: 768,
+    },
+    {
+        label: "tablet_dark",
+        width: 1024,
+        height: 768,
+        prefersColorScheme: "dark"
+    },
+    {
+        label: "fhd",
+        width: 1920,
+        height: 1080,
+    },
+    {
+        label: "fhd_dark",
+        width: 1920,
+        height: 1080,
+        prefersColorScheme: "dark"
     }
 ];
